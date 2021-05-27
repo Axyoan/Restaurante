212eba2a5cd8fac106ffac88644593e7b4fb94f8
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2021-05-27 18:17:18 -0500

    Installed react-modal, added little demo of passing data for the dish modal to show

--- client/src/pages/client/main.js
+++ client/src/pages/client/main.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Modal from 'react-modal';
 import Header from "../../components/header";
 import Button from "../../components/button";
 import DishCard from "../../components/dishCard"
@@ -6,16 +7,52 @@ import ScrollButton from "../../components/scrollButton"
 import { ColumnContainer, RowContainer, StyledHr, StyledH2 } from "../../styles/core"
 import StickyFooterButton from '../../components/stickyFooterButton';
 
-function main() {
-    const handleClick = (e) => {
-        alert("click");
+Modal.setAppElement('#root');
+
+function Main() {
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [dishName, setDishName] = useState("");
+
+    function openModal(data) {
+        setIsOpen(true);
+        setDishName(data);
     }
+    function afterOpenModal() {
+
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     const handleScroll = () => {
         window.scroll({
             top: 0,
             behavior: 'smooth',
         });
     }
+
+    const loadDishes = () => {
+        console.log("loadDishes")
+        return (
+            <ColumnContainer>
+                <DishCard title="Guacamole" price="50" description="Lorem ipsum"
+                    onClick={() => openModal(1)}
+                />
+                <DishCard title="Queso fundido" price="40" description="Lorem ipsum"
+                    onClick={() => openModal(2)}
+                />
+                <DishCard title="Fetuccini" price="125" description="Lorem ipsum"
+                    onClick={() => openModal(3)} />
+            </ColumnContainer >
+        );
+    };
+
+    useEffect(() => {
+        console.log("I have been mounted")
+
+    }, [])
+
     return (
         <>
             <Header />
@@ -27,16 +64,13 @@ function main() {
             <RowContainer>
                 <StyledH2>Menú</StyledH2>
             </RowContainer>
-            <ColumnContainer>
-                <DishCard title="Guacamole" price="50" description="Lorem ipsum"
-                    onClick={handleClick}
-                />
-                <DishCard title="Queso fundido" price="40" description="Lorem ipsum"
-                    onClick={handleClick}
-                />
-                <DishCard title="Fetuccini" price="125" description="Lorem ipsum"
-                    onClick={handleClick} />
-            </ColumnContainer>
+            {loadDishes()}
+
+            <Modal isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}>
+                {dishName}
+            </Modal>
             <ScrollButton onClick={handleScroll}>scroll up</ScrollButton>
             <StickyFooterButton color="blue" text="Ordenar" />
         </>
@@ -44,6 +78,6 @@ function main() {
 }
 
 
-export default main
+export default Main
 
 
